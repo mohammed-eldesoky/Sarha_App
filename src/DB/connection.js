90c5e9fe73e729730475f35c0d5a31e15c73e169
mohamed <mohammedeldesoky77@gmail.com>
2025-12-28 15:45:18 +0200

    `Revert import removal and add error handling to connectDB function`

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,13 +1,27 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+// mongoose.connect(process.env.DB_URL)
+// .then(() => {
+//   console.log("MongoDB connected successfully");
+
+// })
+// .catch((error) => {
+//   console.error("MongoDB connection failed:", error);
+// });
+
+// }
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-mongoose.connect(process.env.DB_URL)
-.then(() => {
-  console.log("MongoDB connected successfully");
-
-})
-.catch((error) => {
-  console.error("MongoDB connection failed:", error);
-});
-
-}
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // 
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+  }
+};
